d094111eba5a596c3220d531b07896a5ff673f9b
NigeParis <nrobinso@student.42.fr>
2026-04-21 16:52:43 +0200

    Starting Module 04

---
+++ Module04/ex00/main.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nrobinso <nrobinso@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/13 15:41:53 by nrobinso          #+#    #+#             */
+/*   Updated: 2026/04/21 16:43:50 by nrobinso         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <util/delay.h>                         // delay for the debounce
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "tools.h"
+#include "uart_lib.h"
+#include "config.h"
+#include "led_lib.h"
+#include "timers.h"
+
+typedef unsigned char uint8_t;      // needed because not using stdlib
+
+
+
+int main(void) {
+        
+    while(1){
+
+    }
+}
